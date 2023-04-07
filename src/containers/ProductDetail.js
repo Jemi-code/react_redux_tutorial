16dee1f7f80176f1eec0ac7c55240b76c857bd7c
Jemi-code <jemimahkwakuyi@gmail.com>
2023-04-07 20:06:54 +0000

    blah 1

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -26,7 +26,7 @@ const ProductDetail = () => {
         return () => {
           dispatch(removeSelectedProduct())
         }
-    }, [productId]);
+    });
     return (
         <div className = "ui grid container">
             {Object.keys(product).length === 0} ? (
@@ -42,7 +42,7 @@ const ProductDetail = () => {
                         <div className = "column rp">
                             <h1>{title}</h1>
                             <h2>
-                                <a className = "ui teal tag label">${price}</a>
+                                <button className = "ui teal tag label">${price}</button>
                             </h2>
                             <h3 className = "ui brown block header">{category}</h3>
                             <p>{description}</p>
